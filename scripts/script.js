e2d7b1cdc8049f466eb67f39077433498b026f4f
Anton Khlebnikov <toni.farsh@icloud.com>
2024-11-19 15:52:24 +0500

    innit commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const popup = document.querySelector('.popup-create-match');
+
+function showPopup() {
+    popup.classList.add('open');
+}
+function hidePopup() {
+    popup.classList.remove('open')
+}
